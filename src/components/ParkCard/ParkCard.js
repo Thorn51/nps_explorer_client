34d982bf28b6fbcbac52b05953368ba522687d55
Matt Thornberry <mtberry16@hotmail.com>
2020-03-13 09:29:39 -0600

    Import React, setup functional component

---
+++ src/components/ParkCard/ParkCard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ParkCard() {
+  return <div></div>;
+}
---
+++ src/components/UserInteractions/UserInteractions.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UserInteractions() {
+  return <div></div>;
+}
