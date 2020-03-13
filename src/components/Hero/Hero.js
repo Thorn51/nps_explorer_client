d38332e77e7658c67eb5b410ca46a042ac55febe
Matt Thornberry <mtberry16@hotmail.com>
2020-03-13 10:47:29 -0600

    Add jsx

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function Hero() {
-  return <div></div>;
+export default function Hero(props) {
+  return (
+    <div class="hero" role="banner">
+      <div class="title_container">
+        <h1 class="title">{props.title}</h1>
+      </div>
+    </div>
+  );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Navigation() {
-  return <div></div>;
+  return (
+    <>
+      <nav>
+        <ul class="navigation_list">
+          <li class="nav_li">
+            <a href="#login">Login</a>
+          </li>
+          <li class="nav_li">
+            <a href="#register">Register</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
