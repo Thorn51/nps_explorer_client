0830a20520948673dc6c058eed3997a0b90eb8e4
Matt Thornberry <mtberry16@hotmail.com>
2020-03-13 09:10:42 -0600

    Add park page route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./routes/HomePage/HomePage";
+import ParkPage from "./routes/ParkPage/ParkPage";
 import LoginPage from "./routes/LoginPage/LoginPage";
 import RegistrationPage from "./routes/RegistrationPage";
 import NoPage from "./routes/NoPage/NoPage";
@@ -10,6 +11,7 @@ function App() {
     <main className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/park/:parkCode" component={ParkPage} />
         <Route path="/login" component={LoginPage} />
         <Route path="/registration" component={RegistrationPage} />
         <Route component={NoPage} />
