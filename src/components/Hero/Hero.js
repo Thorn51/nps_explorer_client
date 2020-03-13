5cda7c71d12d629cb81a7ef4b9afadbfc4377ca7
Matt Thornberry <mtberry16@hotmail.com>
2020-03-13 11:13:07 -0600

    Change class to className

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,9 +3,9 @@ import "./Hero.css";
 
 export default function Hero(props) {
   return (
-    <div class="hero" role="banner">
-      <div class="title_container">
-        <h1 class="title">{props.title}</h1>
+    <div className="hero" role="banner">
+      <div className="title_container">
+        <h1 className="title">{props.title}</h1>
       </div>
     </div>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,11 +5,11 @@ export default function Navigation() {
   return (
     <>
       <nav>
-        <ul class="navigation_list">
-          <li class="nav_li">
+        <ul className="navigation_list">
+          <li className="nav_li">
             <a href="#login">Login</a>
           </li>
-          <li class="nav_li">
+          <li className="nav_li">
             <a href="#register">Register</a>
           </li>
         </ul>
