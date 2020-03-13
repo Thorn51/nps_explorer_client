cc67d8690b0a5f06feb9697accb4408a648d5ae0
Matt Thornberry <mtberry16@hotmail.com>
2020-03-13 08:33:58 -0600

    Import route elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./routes/HomePage/HomePage";
+import LoginPage from "./routes/LoginPage/LoginPage";
+import RegistrationPage from "./routes/RegistrationPage";
+import NoPage from "./routes/NoPage/NoPage";
 import "./App.css";
 
 function App() {
