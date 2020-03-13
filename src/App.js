da1493464084afaf5c2bda0547d153dbc41e23df
Matt Thornberry <mtberry16@hotmail.com>
2020-03-13 08:43:31 -0600

    setup routes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ import NoPage from "./routes/NoPage/NoPage";
 import "./App.css";
 
 function App() {
-  return <main className="App"></main>;
+  return (
+    <main className="App">
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/registration" component={RegistrationPage} />
+        <Route component={NoPage} />
+      </Switch>
+    </main>
+  );
 }
 
 export default App;
