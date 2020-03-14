cce4e665d51d1c2a4cf8635544864b46a7412853
Matt Thornberry <mtberry16@hotmail.com>
2020-03-14 13:32:00 -0600

    Test data model

--- src/routes/ParkPage/ParkPage.js
+++ src/routes/ParkPage/ParkPage.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navigation from "../../components/Navigation/Navigation";
 import Hero from "../../components/Hero/Hero";
+import { GlobalContext } from "../../context/GlobalState";
 import "./ParkPage.css";
 
 export default function ParkPage() {
+  const { parks } = useContext(GlobalContext);
+  console.log(parks[0].data[0].fullName);
   return (
     <>
       <Navigation />
-      <Hero title="Park Name Here" />
+      <Hero title={parks[0].data[0].fullName} />
     </>
   );
 }
