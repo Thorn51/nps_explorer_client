82efb75b20c6e687575173c8e0b12300438c528a
Matt Thornberry <mtberry16@hotmail.com>
2020-03-14 13:30:22 -0600

    Add card jsx

--- src/components/ParkCard/ParkCard.js
+++ src/components/ParkCard/ParkCard.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./ParkCard.css";
 
 export default function ParkCard() {
-  return <div></div>;
+  return (
+    <div class="park_card">
+      <h5 class="pc_designation">National Monument</h5>
+      <h3 class="pc_name">Brimingham Civil Rights</h3>
+      <p class="pc_description">
+        In 1963, images of snarling police dogs unleashed against non-violent
+        protesters and of children being sprayed with high-pressure hoses
+        appeared in print and television news around the world. These dramatic
+        scenes...
+      </p>
+    </div>
+  );
 }
