bf783a4dc634c2c4e46d5d6d31d6b7b437dfc189
Matt Thornberry <mtberry16@hotmail.com>
2020-03-14 13:29:52 -0600

    Add provider for global state

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import LoginPage from "./routes/LoginPage/LoginPage";
 import RegistrationPage from "./routes/RegistrationPage/RegistrationPage";
 import NoPage from "./routes/NoPage/NoPage";
 
+import { GlobalProvider } from "./context/GlobalState";
+
 function App() {
   return (
     <main className="App">
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/park/:parkCode" component={ParkPage} />
-        <Route path="/login" component={LoginPage} />
-        <Route path="/registration" component={RegistrationPage} />
-        <Route component={NoPage} />
-      </Switch>
+      <GlobalProvider>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/park/:parkCode" component={ParkPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/registration" component={RegistrationPage} />
+          <Route component={NoPage} />
+        </Switch>
+      </GlobalProvider>
     </main>
   );
 }
