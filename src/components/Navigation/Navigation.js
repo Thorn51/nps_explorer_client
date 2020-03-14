f7915f177a6ed341888d3e75505e62a840eff709
Matt Thornberry <mtberry16@hotmail.com>
2020-03-14 13:30:09 -0600

    Add home option

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 export default function Navigation() {
@@ -6,6 +7,11 @@ export default function Navigation() {
     <>
       <nav>
         <ul className="navigation_list">
+          <ul className="nav_logo">
+            <li className="nav_li">
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
           <li className="nav_li">
             <a href="#login">Login</a>
           </li>
