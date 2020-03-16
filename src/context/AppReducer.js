221a12409941cdaa3fb5253d92dd1abcd0d82a1e
Matt Thornberry <mtberry16@hotmail.com>
2020-03-16 12:53:37 -0600

    Testing application with reducer

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,22 @@
 export default (state, action) => {
   switch (action.type) {
+    case "SEARCH_STATE":
+      return {
+        ...state,
+        searchState: [action.payload],
+        loading: true
+      };
+    case "PARKS_IN_STATE":
+      return {
+        ...state,
+        parksInState: [action.payload],
+        loading: false
+      };
+    case "FETCH_PARKS_ERROR":
+      return {
+        ...state,
+        error: action.payload
+      };
     default:
       return state;
   }
