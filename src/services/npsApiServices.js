52e1e4b7355a2b060767f5ae1b1870910dbc8653
Matt Thornberry <mtberry16@hotmail.com>
2020-03-16 12:55:05 -0600

    Setup fetch services for nps api

---
+++ src/services/npsApiServices.js
@@ -0,0 +1,53 @@
+import config from "../config";
+
+const npsApiService = {
+  // Get parks by 2 character state codes
+  getParksByState(searchState) {
+    // Send authorization header with api token
+    const headers = {
+      Authorization: config.NPS_API_TOKEN
+    };
+    // Form endpoint
+    let baseUrl = `${config.NPS_API_BASE_URL}/parks?`;
+    let queryParam = `&stateCode=${searchState}`;
+    let endPoint = `${baseUrl}${queryParam}`;
+
+    // Fetch the parks data from NPS api
+    return fetch(endPoint, headers)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch parks");
+        } else {
+          return res.json();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+
+  // Get news releases
+  getNews() {
+    // Send authorization header with api token
+    const headers = {
+      Authorization: config.NPS_API_TOKEN
+    };
+    // Form endpoint
+    let baseUrl = `${config.NPS_API_BASE_URL}/newsrelease`;
+
+    // Fetch the parks data from NPS api
+    return fetch(baseUrl, headers)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch news");
+        } else {
+          return res.json();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+
+export default npsApiService;
