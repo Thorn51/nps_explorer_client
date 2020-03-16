eb73c55c230c9218d2c1d152ab8d8ba74bc5fbf4
Matt Thornberry <mtberry16@hotmail.com>
2020-03-16 12:52:11 -0600

    specify variables

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000",
-  API_TOKEN: process.env.REACT_APP_API_KEY,
+  NPS_API_BASE_URL: "https://api.nps.gov/api/v1",
+  NPS_API_TOKEN: process.env.REACT_APP_API_KEY,
   TOKEN_KEY: "incubator-client-auth-token"
 };
