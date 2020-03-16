2a74d70c654a3619b351e9a9a61475e4963c98b4
Matt Thornberry <mtberry16@hotmail.com>
2020-03-16 12:52:53 -0600

    configure for background styles, add tag

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,10 +3,19 @@ import "./Hero.css";
 
 export default function Hero(props) {
   return (
-    <div className="hero" role="banner">
-      <div className="title_container">
-        <h1 className="title">{props.title}</h1>
+    <>
+      <div
+        className="hero"
+        role="banner"
+        style={{ backgroundImage: `url(${props.background})` }}
+      >
+        <div className="title_container">
+          <h1 className="title">{props.title}</h1>
+        </div>
       </div>
-    </div>
+      <div className="tag">
+        <h3 className="tag_text">{props.tag}</h3>
+      </div>
+    </>
   );
 }
