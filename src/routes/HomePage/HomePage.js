611dd89146b087c7a98d5de6b13735dcbf3ef93d
Matt Thornberry <mtberry16@hotmail.com>
2020-03-17 08:57:07 -0600

    Add loading spiller, incorporate model

--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -1,22 +1,31 @@
 import React, { useContext } from "react";
+import { GlobalContext } from "../../context/GlobalState";
 import Navigation from "../../components/Navigation/Navigation";
 import Hero from "../../components/Hero/Hero";
 import StateSelector from "../../components/StateSelector/StateSelector";
+import ParkList from "../../components/ParkList/ParkList";
 import "./HomePage.css";
-import { GlobalContext } from "../../context/GlobalState";
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 export default function HomePage() {
   const { loading, searchState, parksInState } = useContext(GlobalContext);
 
+  // Conditional render based on status of loading
   function renderParks() {
     if (loading === true) {
       return (
-        <h3 className="news">Fetching Parks for {searchState[0].stateName}</h3>
+        <div className="loading_animation">
+          <h3 className="news">
+            Fetching Parks for {searchState[0].stateName}
+          </h3>
+          <PropagateLoader />
+        </div>
       );
     } else {
       return (
         <h3 className="parks_list">
           {searchState[0].stateName} ({parksInState[0].length})
+          <ParkList />
         </h3>
       );
     }
@@ -29,8 +38,16 @@ export default function HomePage() {
         tag="Find a national park to explore..."
         background="https://www.nps.gov/npgallery/GetAsset/2D26D223-1DD8-B71C-07C923F9B774BBB9/proxy/hires"
       />
-      <StateSelector />
-      {loading === null ? <h3 className="news">News</h3> : renderParks()}
+      <main>
+        <StateSelector />
+        <section className="content">
+          {loading === null ? (
+            <h3 className="news">National Park Service News</h3>
+          ) : (
+            renderParks()
+          )}
+        </section>
+      </main>
     </>
   );
 }
