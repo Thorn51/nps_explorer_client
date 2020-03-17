4ccdd1607fe9fd576c7f6fa34ca3f1f3b93b91d4
Matt Thornberry <mtberry16@hotmail.com>
2020-03-17 08:54:16 -0600

    Add jsx, data model

--- src/components/ParkList/ParkList.js
+++ src/components/ParkList/ParkList.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import ParkCard from "../ParkCard/ParkCard";
 import "./ParkList.css";
+import { GlobalContext } from "../../context/GlobalState";
 
 export default function ParkList() {
-  return <div></div>;
+  const { parksInState } = useContext(GlobalContext);
+
+  return (
+    <section className="featured_parks">
+      {parksInState[0].map(park => (
+        <ParkCard
+          key={park.id}
+          designation={park.designation}
+          name={park.name}
+          description={park.description}
+          parkCode={park.parkCode}
+        />
+      ))}
+    </section>
+  );
 }
