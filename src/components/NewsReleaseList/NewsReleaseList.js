c6fad50079df945bf789a226e773f298f9417d43
Matt Thornberry <mtberry16@hotmail.com>
2020-03-17 13:36:15 -0600

    Add condition for render

--- src/components/NewsReleaseList/NewsReleaseList.js
+++ src/components/NewsReleaseList/NewsReleaseList.js
@@ -1,19 +1,27 @@
 import React, { useContext, useEffect } from "react";
 import { GlobalContext } from "../../context/GlobalState";
 import NewsRelease from "../NewsRelease/NewsRelease";
+import PropagateLoader from "react-spinners/PropagateLoader";
+import "./NewsReleaseList.css";
 
 export default function NewsReleaseList() {
-  const { news, getNews } = GlobalContext(useContext);
-
+  const { news, getNews } = useContext(GlobalContext);
+  // Get news
   useEffect(() => {
     getNews();
   }, []);
 
   console.log(news);
 
-  return (
+  return news.length === 0 ? (
     <section className="nps_news">
-      {news.map(release => (
+      <h3 className="section_title">Fetching National Park Service News</h3>
+      <PropagateLoader />
+    </section>
+  ) : (
+    <section className="nps_news">
+      <h3 className="section_title">National Park Service News</h3>
+      {news[0].map(release => (
         <NewsRelease
           key={release.id}
           title={release.title}
