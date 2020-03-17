00c339290f29cf9b505083a0f66883b4064e74d6
Matt Thornberry <mtberry16@hotmail.com>
2020-03-17 08:57:44 -0600

    Add jsx, model

--- src/routes/ParkPage/ParkPage.js
+++ src/routes/ParkPage/ParkPage.js
@@ -1,16 +1,41 @@
 import React, { useContext } from "react";
+import { GlobalContext } from "../../context/GlobalState";
 import Navigation from "../../components/Navigation/Navigation";
 import Hero from "../../components/Hero/Hero";
-import { GlobalContext } from "../../context/GlobalState";
+
 import "./ParkPage.css";
 
-export default function ParkPage() {
-  const { parks } = useContext(GlobalContext);
-  console.log(parks[0].data[0].fullName);
+export default function ParkPage(props) {
+  const { parksInState } = useContext(GlobalContext);
+
+  // Find the park based on the park code
+  const park = parksInState[0].find(
+    park => park.parkCode === props.match.params.parkCode
+  );
+
+  console.log(park);
+
+  // Generate a random number based on the image array size to use in Hero background
+  const selectImage = Math.floor(Math.random() * park.images.length);
+
   return (
     <>
       <Navigation />
-      <Hero title={parks[0].data[0].fullName} />
+      <Hero
+        title={park.name}
+        background={park.images[selectImage].url}
+        tag={park.designation}
+      />
+      <main className="park_main">
+        <section className="overview">
+          <h3 className="section_title">Description</h3>
+          <p>{park.description}</p>
+        </section>
+        <section className="directions">
+          <h3 className="section_title">Directions</h3>
+          <p>{park.directionsInfo}</p>
+        </section>
+      </main>
     </>
   );
 }
