f429208cba5538971ab27c915024c2370820e6fb
Matt Thornberry <mtberry16@hotmail.com>
2020-03-17 10:39:35 -0600

    Add effect

--- src/components/NewsReleaseList/NewsReleaseList.js
+++ src/components/NewsReleaseList/NewsReleaseList.js
@@ -1,8 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { GlobalContext } from "../../context/GlobalState";
+import NewsRelease from "../NewsRelease/NewsRelease";
 
 export default function NewsReleaseList() {
-  const { news } = GlobalContext(useContext);
+  const { news, getNews } = GlobalContext(useContext);
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
   console.log(news);
-  return <div></div>;
+
+  return (
+    <section className="nps_news">
+      {news.map(release => (
+        <NewsRelease
+          key={release.id}
+          title={release.title}
+          date={release.releasedate}
+          content={release.abstract}
+          url={release.url}
+        />
+      ))}
+    </section>
+  );
 }
