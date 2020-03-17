96621970113c581627691fcd09f70d73c536ba54
Matt Thornberry <mtberry16@hotmail.com>
2020-03-17 09:22:12 -0600

    Fix tags

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Navigation from "../../components/Navigation/Navigation";
 import Hero from "../../components/Hero/Hero";
 import "./LoginPage.css";
@@ -13,8 +14,8 @@ export default function LoginPage() {
         background="https://www.nps.gov/npgallery/GetAsset/34F574D2-1DD8-B71B-0BDE3EA69DD198E8/proxy/hires"
       />
       <main>
-        <form class="login_form">
-          <label for="login_email">Email:</label>
+        <form className="login_form">
+          <label htmlFor="login_email">Email:</label>
           <input
             type="text"
             placeholder="Enter Email"
@@ -22,7 +23,7 @@ export default function LoginPage() {
             id="login_email"
             required
           />
-          <label for="login_password">Password:</label>
+          <label htmlFor="login_password">Password:</label>
           <input
             type="password"
             placeholder="Enter Password"
@@ -30,15 +31,15 @@ export default function LoginPage() {
             id="login_password"
             required
           />
-          <button type="submit" class="form_button">
+          <button type="submit" className="form_button">
             Login
           </button>
         </form>
-        <div class="form_redirect">
+        <div className="form_redirect">
           <hr />
           <p>
-            Do you need to create an account? Visit the
-            <a href="#registration">registration page</a>.
+            Do you need to create an account? Visit the {""}
+            <Link to="/registration">registration page.</Link>.
           </p>
         </div>
       </main>
