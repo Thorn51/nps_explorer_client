cd6b215ff286d08252380acb65b70ef439c2aabf
Matt Thornberry <mtberry16@hotmail.com>
2020-03-17 18:18:44 -0600

    Fixing alignment issues

--- src/components/ParkList/ParkList.js
+++ src/components/ParkList/ParkList.js
@@ -4,19 +4,24 @@ import ParkCard from "../ParkCard/ParkCard";
 import "./ParkList.css";
 
 export default function ParkList() {
-  const { parksInState } = useContext(GlobalContext);
+  const { parksInState, searchState } = useContext(GlobalContext);
 
   return (
-    <section className="featured_parks">
-      {parksInState[0].map(park => (
-        <ParkCard
-          key={park.id}
-          designation={park.designation}
-          name={park.name}
-          description={park.description}
-          parkCode={park.parkCode}
-        />
-      ))}
-    </section>
+    <div className="parks_container">
+      <h3 className="parks_list_header">
+        {searchState[0].stateName} ({parksInState[0].length})
+      </h3>
+      <section className="featured_parks">
+        {parksInState[0].map(park => (
+          <ParkCard
+            key={park.id}
+            designation={park.designation}
+            name={park.name}
+            description={park.description}
+            parkCode={park.parkCode}
+          />
+        ))}
+      </section>
+    </div>
   );
 }
