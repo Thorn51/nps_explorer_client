47a15c4cf791fb21fbd74d0cc1212b6dd5585496
Matt Thornberry <mtberry16@hotmail.com>
2020-03-18 04:51:42 -0600

    Form alignment and size

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -10,37 +10,37 @@ export default function LoginPage() {
       <Navigation />
       <Hero
         title="National Parks Explorer"
-        tag="Login"
+        tag="Login to your account"
         background="https://www.nps.gov/npgallery/GetAsset/34F574D2-1DD8-B71B-0BDE3EA69DD198E8/proxy/hires"
       />
       <main>
-        <form className="login_form">
-          <label htmlFor="login_email">Email:</label>
-          <input
-            type="text"
-            placeholder="Enter Email"
-            name="email"
-            id="login_email"
-            required
-          />
-          <label htmlFor="login_password">Password:</label>
-          <input
-            type="password"
-            placeholder="Enter Password"
-            name="password"
-            id="login_password"
-            required
-          />
-          <button type="submit" className="form_button">
-            Login
-          </button>
-        </form>
-        <div className="form_redirect">
-          <hr />
-          <p>
-            Do you need to create an account? Visit the {""}
-            <Link to="/registration">registration page.</Link>.
-          </p>
+        <div className="form_container">
+          <form className="login_form">
+            <label htmlFor="login_email">Email:</label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              id="login_email"
+              required
+            />
+            <label htmlFor="login_password">Password:</label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              id="login_password"
+              required
+            />
+            <button type="submit" className="form_button">
+              Login
+            </button>
+            <hr />
+            <p>
+              Do you need to create an account? Visit the {""}
+              <Link to="/registration">registration page.</Link>.
+            </p>
+          </form>
         </div>
       </main>
     </>
--- src/routes/RegistrationPage/RegistrationPage.js
+++ src/routes/RegistrationPage/RegistrationPage.js
@@ -10,61 +10,63 @@ export default function RegistrationPage() {
       <Navigation />
       <Hero
         title="National Parks Explorer"
-        tag="Register"
+        tag="Register for an account today!"
         background="https://www.nps.gov/npgallery/GetAsset/268E719A-155D-451F-675B13A1AC17C77E/proxy/hires"
       />
       <main>
-        <form className="registration_form">
-          <label htmlFor="registration_first_name">First Name:</label>
-          <input
-            type="text"
-            name="first_name"
-            id="registration_first_name"
-            placeholder="Enter First Name"
-            required
-          />
-          <label htmlFor="registration_last_name">Last Name:</label>
-          <input
-            type="text"
-            name="last_name"
-            id="registration_last_name"
-            placeholder="Enter Last Name"
-            required
-          />
-          <label htmlFor="registration_email">Email:</label>
-          <input
-            type="text"
-            placeholder="Enter Email"
-            name="email"
-            id="registration_email"
-            required
-          />
-          <label htmlFor="registration_password">Password:</label>
-          <input
-            type="password"
-            placeholder="Enter Password"
-            name="password"
-            id="registration_password"
-            required
-          />
-          <label htmlFor="registration_repeat_password">Repeat Password:</label>
-          <input
-            type="password"
-            name="repeat_password"
-            id="registration_repeat_password"
-            placeholder="Repeat Password"
-            required
-          />
-          <button type="submit" className="form_button">
-            Register
-          </button>
-        </form>
-        <div className="form_redirect">
-          <hr />
-          <p>
-            Do you already have an account? Visit the{" "}
-            <Link to="/login">login page.</Link>
-          </p>
+        <div className="form_container">
+          <form className="registration_form">
+            <label htmlFor="registration_first_name">First Name:</label>
+            <input
+              type="text"
+              name="first_name"
+              id="registration_first_name"
+              placeholder="Enter First Name"
+              required
+            />
+            <label htmlFor="registration_last_name">Last Name:</label>
+            <input
+              type="text"
+              name="last_name"
+              id="registration_last_name"
+              placeholder="Enter Last Name"
+              required
+            />
+            <label htmlFor="registration_email">Email:</label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              id="registration_email"
+              required
+            />
+            <label htmlFor="registration_password">Password:</label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              id="registration_password"
+              required
+            />
+            <label htmlFor="registration_repeat_password">
+              Repeat Password:
+            </label>
+            <input
+              type="password"
+              name="repeat_password"
+              id="registration_repeat_password"
+              placeholder="Repeat Password"
+              required
+            />
+            <button type="submit" className="form_button">
+              Register
+            </button>
+            <hr />
+            <p>
+              Do you already have an account? Visit the{" "}
+              <Link to="/login">login page.</Link>
+            </p>
+          </form>
         </div>
       </main>
     </>
