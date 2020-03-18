2759bbd820b30f56604ef282e0ea4e5a4226a131
Matt Thornberry <mtberry16@hotmail.com>
2020-03-18 11:30:56 -0600

    Add jsx

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../../context/GlobalState";
+import Comment from "../Comment/Comment";
+import CommentForm from "../../components/CommentForm/CommentForm";
 import "./CommentList.css";
 
-export default function CommentList() {
-  return <div></div>;
+export default function CommentList(props) {
+  const { comments } = useContext(GlobalContext);
+  let parkComments = comments.filter(
+    comment => comment.parkCode === props.parkCode
+  );
+  return (
+    <section className="feedback">
+      <h3 className="section_title">Comments</h3>
+      {parkComments.map(comment => (
+        <Comment
+          key={comment.id}
+          author={comment.author}
+          date={comment.dateSubmitted}
+          comment={comment.comment}
+        />
+      ))}
+      <CommentForm parkCode={props.parkCode} parkName={props.parkName} />
+    </section>
+  );
 }
