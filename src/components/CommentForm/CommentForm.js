cd8238b66fc553beaff394ffee0fb337003ec1c8
Matt Thornberry <mtberry16@hotmail.com>
2020-03-18 08:11:08 -0600

    Setup controlled form

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -1,9 +1,25 @@
-import React, { useContext } from "react";
-import GlobalState from "../../context/GlobalState";
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "../../context/GlobalState";
 
 export default function CommentForm(props) {
+  const { postComment } = useContext(GlobalContext);
+
+  const [comment, setComment] = useState("");
+
+  const onSubmit = e => {
+    e.preventDefault();
+    let newComment = {
+      comment,
+      author: "Author",
+      dateSubmitted: Date.now().toString(),
+      parkCode: props.parkCode
+    };
+    postComment(newComment);
+    setComment("");
+  };
+
   return (
-    <form className="comment_form">
+    <form className="comment_form" onSubmit={onSubmit}>
       <p>You are required to be logged in to submit comments.</p>
       <hr />
       <label htmlFor="comment">Leave your thoughts for {props.parkName}</label>
@@ -13,6 +29,8 @@ export default function CommentForm(props) {
         rows="15"
         className="comment"
         id="comment"
+        value={comment}
+        onChange={e => setComment(e.target.value)}
       ></textarea>
       <button type="submit" className="form_button">
         Submit
