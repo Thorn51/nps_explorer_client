526de47ca2bd501f40d5d6360276a7580d273f24
Matt Thornberry <mtberry16@hotmail.com>
2020-03-17 21:23:02 -0600

    Add jsx

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -1,6 +1,22 @@
 import React, { useContext } from "react";
 import GlobalState from "../../context/GlobalState";
 
-export default function CommentForm() {
-  return <div></div>;
+export default function CommentForm(props) {
+  return (
+    <form className="comment_form">
+      <p>You are required to be logged in to submit comments.</p>
+      <hr />
+      <label htmlFor="comment">Leave your thoughts for {props.parkName}</label>
+      <textarea
+        name="comment"
+        cols="30"
+        rows="15"
+        className="comment"
+        id="comment"
+      ></textarea>
+      <button type="submit" className="form_button">
+        Submit
+      </button>
+    </form>
+  );
 }
