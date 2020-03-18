578747ce70945b5ec96b7c895d264a92de32a4f8
Matt Thornberry <mtberry16@hotmail.com>
2020-03-17 18:20:17 -0600

    Refactor to pull data independently

--- src/routes/ParkPage/ParkPage.js
+++ src/routes/ParkPage/ParkPage.js
@@ -1,41 +1,63 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { GlobalContext } from "../../context/GlobalState";
 import Navigation from "../../components/Navigation/Navigation";
 import Hero from "../../components/Hero/Hero";
-
+import PropagateLoader from "react-spinners/PropagateLoader";
 import "./ParkPage.css";
 
 export default function ParkPage(props) {
-  const { parksInState } = useContext(GlobalContext);
+  const { getParkByParkCode, park } = useContext(GlobalContext);
 
   // Find the park based on the park code
-  const park = parksInState[0].find(
-    park => park.parkCode === props.match.params.parkCode
-  );
+  const parkCode = props.match.params.parkCode;
 
-  console.log(park);
+  // Fetch the data from NPS and add it to the global state
+  useEffect(() => {
+    getParkByParkCode(parkCode);
+  }, []);
 
-  // Generate a random number based on the image array size to use in Hero background
-  const selectImage = Math.floor(Math.random() * park.images.length);
+  // Wait for data
+  function renderPage() {
+    if (park.length === 0) {
+      return (
+        <>
+          <Navigation />
+          <Hero title={park.name} background={null} tag={park.designation} />
+          <div className="loader">
+            <h3 className="section_title">Fetching Park Details</h3>
+            <div className="loader_animation">
+              <PropagateLoader />
+            </div>
+          </div>
+        </>
+      );
+    } else {
+      // Generate a random number based on the image array size to use in Hero background
+      const selectImage = Math.floor(Math.random() * park.images.length);
+      return (
+        <>
+          <Navigation />
+          <Hero
+            title={park.name}
+            background={
+              park.images.length > 0 ? park.images[selectImage].url : null
+            }
+            tag={park.designation}
+          />
+          <main className="park_main">
+            <section className="overview">
+              <h3 className="section_title">Description</h3>
+              <p>{park.description}</p>
+            </section>
+            <section className="directions">
+              <h3 className="section_title">Directions</h3>
+              <p>{park.directionsInfo}</p>
+            </section>
+          </main>
+        </>
+      );
+    }
+  }
 
-  return (
-    <>
-      <Navigation />
-      <Hero
-        title={park.name}
-        background={park.images[selectImage].url}
-        tag={park.designation}
-      />
-      <main className="park_main">
-        <section className="overview">
-          <h3 className="section_title">Description</h3>
-          <p>{park.description}</p>
-        </section>
-        <section className="directions">
-          <h3 className="section_title">Directions</h3>
-          <p>{park.directionsInfo}</p>
-        </section>
-      </main>
-    </>
-  );
+  return renderPage();
 }
