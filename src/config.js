952396fb3e1f36d2fa9233cdc630bf6ff204f119
Matt Thornberry <mtberry16@hotmail.com>
2020-03-26 09:24:17 -0600

    Configure explorer enpoint details

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
   NPS_API_BASE_URL: "https://api.nps.gov/api/v1",
-  NPS_API_TOKEN: process.env.REACT_APP_API_KEY,
-  TOKEN_KEY: "incubator-client-auth-token"
+  NPS_API_TOKEN: process.env.REACT_APP_NPS_API_TOKEN,
+  EXPLORER_BASE_URL: "https://boiling-anchorage-66066.herokuapp.com",
+  EXPLORER_API_TOKEN: process.env.REACT_APP_EXPLORER_API_TOKEN
 };
