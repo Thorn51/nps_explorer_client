55c71aa916d9c478a6ed3a8e85e2972d9c843c51
Matt Thornberry <mtberry16@hotmail.com>
2020-03-26 10:33:27 -0600

    Add login method

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -1,3 +1,16 @@
 import config from "../config";
 
-const AuthApiService = {};
+const AuthApiService = {
+  postLogin(credentials) {
+    return fetch(`${config.EXPLORER_BASE_URL}/api/auth/login`, {
+      method: "Post",
+      headers: {
+        "content-type": "application/json",
+        Authorization: config.EXPLORER_API_TOKEN
+      },
+      body: JSON.stringify(credentials).then(res => {
+        !res.ok ? res.json().then(e => Promise.reject(e)) : res.json();
+      })
+    });
+  }
+};
