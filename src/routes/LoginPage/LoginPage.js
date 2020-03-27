e3dd785f51b4c858f31d1f795f11268a0707c2db
Matt Thornberry <mtberry16@hotmail.com>
2020-03-26 20:07:58 -0600

    Wire login to api

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "../../context/GlobalState";
 import { Link } from "react-router-dom";
 import Navigation from "../../components/Navigation/Navigation";
 import Hero from "../../components/Hero/Hero";
 import "./LoginPage.css";
 
-export default function LoginPage() {
+export default function LoginPage(props) {
+  const { login } = useContext(GlobalContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const credentials = {
+      email,
+      password
+    };
+
+    login(credentials).then(() => {
+      setEmail("");
+      setPassword("");
+      props.history.goBack();
+    });
+  };
+
   return (
     <>
       <Navigation />
@@ -14,7 +35,7 @@ export default function LoginPage() {
         background="https://www.nps.gov/npgallery/GetAsset/34F574D2-1DD8-B71B-0BDE3EA69DD198E8/proxy/hires"
       />
       <main>
-        <div className="form_container">
+        <div className="form_container" onSubmit={handleSubmit}>
           <form className="login_form">
             <label htmlFor="login_email">Email:</label>
             <input
@@ -22,6 +43,8 @@ export default function LoginPage() {
               placeholder="Enter Email"
               name="email"
               id="login_email"
+              onChange={e => setEmail(e.target.value)}
+              value={email}
               required
             />
             <label htmlFor="login_password">Password:</label>
@@ -30,6 +53,8 @@ export default function LoginPage() {
               placeholder="Enter Password"
               name="password"
               id="login_password"
+              onChange={e => setPassword(e.target.value)}
+              value={password}
               required
             />
             <button type="submit" className="form_button">
