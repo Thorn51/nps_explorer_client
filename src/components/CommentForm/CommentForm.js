1476d2575c938264a384bff40731be1ef0997d01
Matt Thornberry <mtberry16@hotmail.com>
2020-03-26 20:06:18 -0600

    Add conditional render bas on has auth

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -1,46 +1,75 @@
 import React, { useContext, useState } from "react";
 import { GlobalContext } from "../../context/GlobalState";
+import TokenServices from "../../services/token-service";
 import moment from "moment";
 
 export default function CommentForm(props) {
-  const { postComment, comments } = useContext(GlobalContext);
+  const { postComment } = useContext(GlobalContext);
 
   const [comment, setComment] = useState("");
-
-  let id = comments.length === 0 ? 0 : comments[comments.length - 1].id + 1;
 
   const onSubmit = e => {
     e.preventDefault();
     let newComment = {
-      id,
-      comment,
       author: "Author Name",
-      dateSubmitted: moment(),
       parkCode: props.parkCode
     };
     postComment(newComment);
     setComment("");
   };
 
+  const renderFormLoggedIn = () => {
+    return (
+      <form className="comment_form" onSubmit={onSubmit}>
+        <label htmlFor="comment">
+          Did you visit? What did you think of {props.parkName}?
+        </label>
+        <textarea
+          name="comment"
+          cols="30"
+          rows="15"
+          className="comment"
+          id="comment"
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+        ></textarea>
+        <button
+          title="You need to log in to submit comments."
+          type="submit"
+          className="form_button"
+        >
+          Submit
+        </button>
+      </form>
+    );
+  };
+
+  const renderDisabledForm = () => {
+    return (
+      <form className="comment_form">
+        <label htmlFor="comment">
+          Did you visit {props.parkName}? Log in to submit your thoughts.
+        </label>
+        <textarea
+          name="comment"
+          cols="30"
+          rows="15"
+          className="comment"
+          id="comment"
+          disabled
+        ></textarea>
+        <button type="submit" className="form_button" disabled>
+          Submit
+        </button>
+      </form>
+    );
+  };
+
   return (
-    <form className="comment_form" onSubmit={onSubmit}>
-      <label htmlFor="comment">
-        Did you visit? What did you think of {props.parkName}?
-      </label>
-      <textarea
-        name="comment"
-        cols="30"
-        rows="15"
-        className="comment"
-        id="comment"
-        value={comment}
-        onChange={e => setComment(e.target.value)}
-      ></textarea>
-      <button type="submit" className="form_button">
-        Submit
-      </button>
-      <hr />
-      <p>You are required to be logged in to submit comments.</p>
-    </form>
+    <>
+      {TokenServices.hasAuthToken()
+        ? renderFormLoggedIn()
+        : renderDisabledForm()}
+    </>
   );
 }
