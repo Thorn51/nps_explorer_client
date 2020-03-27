b5d9683e618dac1e11c476bba07b4d233d2b49a5
Matt Thornberry <mtberry16@hotmail.com>
2020-03-27 07:20:44 -0600

    Add favorite methods GET, POST,  PATCH

--- src/services/explorerApiServices.js
+++ src/services/explorerApiServices.js
@@ -1,15 +1,19 @@
 import config from "../config";
+import TokenServices from "./token-service";
 
-const explorerApiService = {
+const ExplorerApiService = {
+  // Get all comments
   getComments() {
-    const headers = {
-      Authorization: config.EXPLORER_API_TOKEN
-    };
-
     let baseUrl = config.EXPLORER_BASE_URL;
     let endpoint = `/api/comments`;
 
-    return fetch(baseUrl + endpoint, headers)
+    return fetch(baseUrl + endpoint, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${config.EXPLORER_API_TOKEN}`
+      }
+    })
       .then(res => {
         if (!res.ok) {
           throw new Error("Failed to fetch comments");
@@ -20,7 +24,102 @@ const explorerApiService = {
       .catch(err => {
         console.log(err);
       });
+  },
+  // Post a new comment to database
+  postComment(newComment) {
+    let baseUrl = config.EXPLORER_BASE_URL;
+    let endpoint = `/api/comments`;
+
+    return fetch(baseUrl + endpoint, {
+      method: "POST",
+      body: JSON.stringify(newComment),
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${TokenServices.getAuthToken()}`
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Failed to post comment");
+        } else {
+          return res.json();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  // Get all favorites
+  getFavorites() {
+    let baseUrl = config.EXPLORER_BASE_URL;
+    let endpoint = `/api/favorites`;
+
+    return fetch(baseUrl + endpoint, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${TokenServices.getAuthToken()}`
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch favorites");
+        } else {
+          return res.json();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  // Post a favorite to database
+  postFavorite(favorite) {
+    let baseUrl = config.EXPLORER_BASE_URL;
+    let endpoint = `/api/favorites`;
+
+    return fetch(baseUrl + endpoint, {
+      method: "POST",
+      body: JSON.stringify(favorite),
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${TokenServices.getAuthToken()}`
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Failed to post favorite");
+        } else {
+          return res.json();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  // Edit favorite in database
+  patchFavorite(favoriteId, updateFavorite) {
+    let baseUrl = config.EXPLORER_BASE_URL;
+    let endpoint = `/api/favorites/${favoriteId}`;
+
+    return fetch(baseUrl + endpoint, {
+      method: "PATCH",
+      body: JSON.stringify(updateFavorite),
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${TokenServices.getAuthToken()}`
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Failed to patch favorite");
+        } else {
+          return res.json();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 };
 
-export default explorerApiService;
+export default ExplorerApiService;
