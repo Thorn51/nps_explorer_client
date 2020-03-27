d8c0544470da8e04a4d4043f902b2fbb333fe435
Matt Thornberry <mtberry16@hotmail.com>
2020-03-27 07:26:52 -0600

    Wire up to API

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -1,26 +1,61 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { GlobalContext } from "../../context/GlobalState";
 import Comment from "../Comment/Comment";
 import CommentForm from "../../components/CommentForm/CommentForm";
 import "./CommentList.css";
 
 export default function CommentList(props) {
-  const { comments } = useContext(GlobalContext);
-  let parkComments = comments.filter(
-    comment => comment.parkCode === props.parkCode
-  );
+  const { comments, getComments } = useContext(GlobalContext);
+
+  let [isBusy, setBusy] = useState(true);
+
+  // Get comments from database
+  useEffect(() => {
+    getComments().then(() => {
+      setBusy(false);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const renderParkComments = () => {
+    let parkComments = comments.filter(
+      comment => comment.parkCode === props.parkCode
+    );
+    if (parkComments.length > 0) {
+      return (
+        <section className="feedback">
+          <h3 className="section_title">Comments</h3>
+          {parkComments.map(comment => (
+            <Comment
+              key={comment.id}
+              author={comment.authorName}
+              date={comment.dateSubmitted}
+              comment={comment.commentText}
+            />
+          ))}
+          <CommentForm parkCode={props.parkCode} parkName={props.parkName} />
+        </section>
+      );
+    } else {
+      return (
+        <section className="feedback">
+          <h3 className="section_title">Comments</h3>
+          <p>Be the first to comment.</p>
+          <CommentForm parkCode={props.parkCode} parkName={props.parkName} />
+        </section>
+      );
+    }
+  };
+
   return (
-    <section className="feedback">
-      <h3 className="section_title">Comments</h3>
-      {parkComments.map(comment => (
-        <Comment
-          key={comment.id}
-          author={comment.author}
-          date={comment.dateSubmitted}
-          comment={comment.comment}
-        />
-      ))}
-      <CommentForm parkCode={props.parkCode} parkName={props.parkName} />
-    </section>
+    <>
+      {isBusy === false ? (
+        renderParkComments()
+      ) : (
+        <section className="feedback">
+          <h3 className="section_title">Gathering Comments</h3>
+        </section>
+      )}
+    </>
   );
 }
