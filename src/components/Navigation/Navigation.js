91dedbd4f5dc65a3a60bf48dc84fe438bec3e0bf
Matt Thornberry <mtberry16@hotmail.com>
2020-03-26 20:06:46 -0600

    Add conditional render based off user auth

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import TokenServices from "../../services/token-service";
 import "./Navigation.css";
 
 export default function Navigation() {
-  return (
-    <>
+  const handleLogOut = () => {
+    TokenServices.clearAuthToken();
+  };
+
+  const renderLogoutNav = () => {
+    return (
+      <nav>
+        <ul className="navigation_list">
+          <ul className="nav_logo">
+            <li className="nav_li">
+              <Link to="/">Home</Link>
+            </li>
+          </ul>
+          <li className="nav_li">
+            <Link onClick={handleLogOut} to="/">
+              Logout
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  };
+
+  const renderLoginNav = () => {
+    return (
       <nav>
         <ul className="navigation_list">
           <ul className="nav_logo">
@@ -20,6 +44,9 @@ export default function Navigation() {
           </li>
         </ul>
       </nav>
-    </>
+    );
+  };
+  return (
+    <>{TokenServices.hasAuthToken() ? renderLogoutNav() : renderLoginNav()}</>
   );
 }
