708b1c3aa669b6cfcc058723ddf522c179250e19
Matt Thornberry <mtberry16@hotmail.com>
2020-03-29 20:56:56 -0600

    Add provider

--- src/components/CommentList/CommentList.test.js
+++ src/components/CommentList/CommentList.test.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
 import CommentList from "./CommentList";
+import { GlobalProvider } from "../../context/GlobalState";
 
 describe("<CommentList />", () => {
   it("Renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<CommentList />, div);
+    ReactDOM.render(
+      <GlobalProvider>
+        <CommentList />
+      </GlobalProvider>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 
