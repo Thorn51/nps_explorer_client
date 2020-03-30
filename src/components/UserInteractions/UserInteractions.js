e774774333ca6089693c7ec231657c04e30829da
Matt Thornberry <mtberry16@hotmail.com>
2020-03-29 20:10:18 -0600

    Wire up favorite icon

--- src/components/UserInteractions/UserInteractions.js
+++ src/components/UserInteractions/UserInteractions.js
@@ -1,30 +1,57 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Favorite } from "@material-ui/icons";
 import "./UserInteractions.css";
+import TokenServices from "../../services/token-service";
 import { GlobalContext } from "../../context/GlobalState";
 
 export default function UserInteractions(props) {
-  const [favoritePark, setFavorite] = useState(0);
+  const [isFavorite, setIsFavorite] = useState();
+  const [hasFavorite, setHasFavorite] = useState();
+  const { addFavorite, patchFavorite } = useContext(GlobalContext);
 
-  const { favorite, addFavorite } = useContext(GlobalContext);
+  useEffect(() => {
+    let user = TokenServices.getUsersDetails(TokenServices.getAuthToken());
+    let userId = user.userId;
+    let hasUserFavorite = props.favorites.find(
+      favorite => favorite.userAccount === userId
+    );
+    hasUserFavorite ? setHasFavorite(true) : setHasFavorite(false);
+    hasUserFavorite
+      ? setIsFavorite(hasUserFavorite.favorite)
+      : setIsFavorite(false);
+  }, [props.favorites]);
 
-  let id = favorite.length === 0 ? 0 : favorite.length + 1;
+  console.log(isFavorite);
 
-  const onClick = () => {
-    setFavorite({
-      id: id,
-      selected: !favoritePark.selected,
-      parkCode: props.parkCode
-    });
-
-    addFavorite(favoritePark);
+  const onClick = e => {
+    e.preventDefault();
+    if (hasFavorite === false) {
+      let newFavorite = {
+        parkCode: props.parkCode,
+        favorite: true
+      };
+      addFavorite(newFavorite);
+      // setHasFavorite(true);
+      // setIsFavorite(true);
+    } else {
+      let user = TokenServices.getUsersDetails(TokenServices.getAuthToken());
+      let userId = user.userId;
+      let userFavorite = {
+        ...props.favorites.filter(favorite => favorite.userAccount === userId)
+      };
+      let updateField = {
+        favorite: !isFavorite
+      };
+      patchFavorite(userFavorite[0].id, updateField);
+      setIsFavorite(!isFavorite);
+    }
   };
 
   const favoriteIcon =
-    favoritePark.selected === true ? (
-      <Favorite onClick={onClick} color="secondary" fontSize="large" />
-    ) : (
+    isFavorite === false ? (
       <Favorite onClick={onClick} fontSize="large" />
+    ) : (
+      <Favorite onClick={e => onClick(e)} color="secondary" fontSize="large" />
     );
 
   return <div className="user_interactions">{favoriteIcon}</div>;
